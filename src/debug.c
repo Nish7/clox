7b9c93e08f1591b2b6898f7723fb5596b2bafb6e
Nishil <nishilkapadia17@gmail.com>
2025-06-11 21:34:05 -0400

    add disassembler, binary op, negation op, vm support added

--- src/debug.c
+++ src/debug.c
@@ -20,11 +20,11 @@ void disassembleChunk(Chunk *chunk, const char *name) {
   printf("== %s ==\n", name);
 
   for (int offset = 0; offset < chunk->count;) {
-    offset = disassembleIntruction(chunk, offset);
+    offset = disassembleInstruction(chunk, offset);
   }
 }
 
-int disassembleIntruction(Chunk *chunk, int offset) {
+int disassembleInstruction(Chunk *chunk, int offset) {
   printf("%04d ", offset);
 
   if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
@@ -39,6 +39,16 @@ int disassembleIntruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_RETURN", offset);
   case OP_CONSTANT:
     return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
+  case OP_ADD:
+    return simpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:
+    return simpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:
+    return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return simpleInstruction("OP_DIVIDE", offset);
   default:
     printf("unknown opcode %d\n", instruction);
     return offset + 1;
