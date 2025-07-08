2c149e2c94e745ba76fbbdc41737c26d548be7d3
Nishil <nishilkapadia17@gmail.com>
2025-07-07 23:17:02 -0400

    logical operators, conditionals and loop constructos

--- src/vm.c
+++ src/vm.c
@@ -66,6 +66,8 @@ static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT());
+#define READ_SHORT() (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | (vm.ip[-1])))
+
 #define BINARY_OP(valueType, op)                                               \
   do {                                                                         \
     if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                          \
@@ -193,6 +195,22 @@ static InterpretResult run() {
       vm.stack[slot] = peek(0);
       break;
     }
+    case OP_JUMP_IF_FALSE: {
+      uint8_t offset = READ_SHORT();
+      if (isFalsey(peek(0)))
+        vm.ip += offset;
+      break;
+    }
+    case OP_JUMP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip += offset;
+      break;
+    }
+    case OP_LOOP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
     case OP_POP: {
       pop();
       break;
@@ -203,6 +221,7 @@ static InterpretResult run() {
 #undef READ_BYTE
 #undef READ_CONSTANT
 #undef READ_STRING
+#undef READ_SHORT
 #undef BINARY_OP
 }
 
